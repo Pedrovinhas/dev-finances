291d578ac0038420b211961e5b1089e855f0c0be
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-22 00:10:39 -0300

    utils correction

--- scripts.js
+++ scripts.js
@@ -117,12 +117,11 @@ const DOM = {
 }
 
 const Utils = {
-    formatAmount(value){
-        value = Number(value.replace(/\,\./g, "")) * 100
-        
-        return value
-    },
-
+    formatAmount(value) {
+        value = value * 100;
+        return Math.round(value);
+      },
+      
     formatDate(date) {
         const splittedDate = date.split("-")
         return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
